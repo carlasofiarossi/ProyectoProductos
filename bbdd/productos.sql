633ee944097330009c6cab6c21012cfc24060c3c
Carla <41465737@ifts24.edu.ar>
2025-07-12 17:42:47 -0300

    actualizando bbdd

--- bbdd/productos.sql
+++ bbdd/productos.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2024 a las 03:59:03
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 12-07-2025 a las 22:42:04
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,23 +35,24 @@ CREATE TABLE `articulos` (
   `FK_Tipo` int(10) NOT NULL,
   `FK_Marca` int(10) DEFAULT NULL,
   `Descripcion` varchar(500) NOT NULL,
-  `Precio` float NOT NULL
+  `Precio` float NOT NULL,
+  `Imagen` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `articulos`
 --
 
-INSERT INTO `articulos` (`ID_Articulo`, `Nombre`, `FK_Categoria`, `FK_Subcategoria`, `FK_Tipo`, `FK_Marca`, `Descripcion`, `Precio`) VALUES
-(1, 'Brillo Labial Lifter Gloss', 1, 1, 1, 1, 'Este gloss con ácido hialurónico mantiene tus labios hidratados hasta 4 horas. Esto significa que el gloss le dará ese efecto volumen a tus labios gracias a su brillo, y también mantendrá su humectación durante este periodo de tiempo. Eso sí, las comidas, y las bebidas podrían modificar esta duración. Por eso, cuando sientas que el brillo de labios haya disminuido, no tengas miedo en volver a aplicar una capa nueva para que el gloss haga su efecto.', 11702),
-(2, 'Base True Match Serum', 1, 2, 2, 2, 'True Match Nude Hyaluronic Tinted Serum, al instante ilumina, equilibra e hidrata, todo en uno para un radiante acabado en tono piel. 1% de ácido hialurónico, hidrata y suaviza al instante, pigmentos minerales luminosos para una cobertura ligera que ilumina tu aspecto, el ácido hialurónico se encuentra naturalmente en la piel y mejora la retención de humedad de la piel.', 25289),
-(3, 'Polvo Natural Bronzer', 1, 2, 3, 5, 'Este bronceador Rimmel London Natural Bronzer está diseñado para proporcionar un brillo cálido en todo el cuerpo y un aspecto natural y bronceado. Aplica el bronceador en las mejillas y las sienes para crear un brillo cálido y natural.\r\n', 17500),
-(4, 'Sombra en Barra Metallic Eyeshadow', 1, 3, 4, 6, 'Súper cremosa y de larga duración. Esta sombra en barra logra excelente cobertura en una sola pasada, mientras se siente liviana y confortable. Práctica a la hora de aplicar y trasladar, aporta un efecto metálico único. Probá las sombras con colores más intensos y brillantes. ', 12490),
-(5, 'Essencial Exclusivo Floral Eau de Parfum Femenino', 2, 2, 5, 7, 'Creación moderna, fuerte y al mismo tiempo misteriosa. Combina las especias frescas con el cuerpo de la madera y los ingredientes naturales inéditos brasileños. Un perfume exclusivo, que hace cada momento único. Camino olfativo:\r\nIntenso, jazmin, osmanthus, ylang ylang', 56469),
-(6, 'Labial CC Hidratante FPS 25 Una', 1, 1, 6, 7, 'El Labial CC Hidratante FPS 25 de Una posee colores llamativos, además de tratar tus labios con 10 beneficios increíbles. Contiene ingredientes de origen natural (sésamo, cacao y ceramidas de maracuyá), que combaten el envejecimiento y contribuyen para la firmeza, el volumen y la nutrición de los labios.', 21410),
-(7, 'Paleta de Sombras 12 Colores Starlight', 1, 3, 4, 6, 'Paleta de Sombras Extreme 12 Colores Starlight. Tener presente que algunos productos pueden venir sin termosellado.', 11693),
-(8, 'Superstay Vinyl Ink Unrivaled', 1, 1, 7, 1, 'El nuevo labial líquido SuperStay Vinyl Ink de Maybellline New York leda a tus labios un color vinilo de larga duración. Ahora disponible ennuevos tonos nude. Consigue un brillo instantáneo que dura hasta 16horas.', 12803),
-(15, 'Articulofalso', 1, 1, 1, 1, 'iodfgsfvdsfffdsfsfdfsfddsf', 1234);
+INSERT INTO `articulos` (`ID_Articulo`, `Nombre`, `FK_Categoria`, `FK_Subcategoria`, `FK_Tipo`, `FK_Marca`, `Descripcion`, `Precio`, `Imagen`) VALUES
+(1, 'Brillo Labial Lifter Gloss', 1, 1, 1, 1, 'Este gloss con ácido hialurónico mantiene tus labios hidratados hasta 4 horas. Esto significa que el gloss le dará ese efecto volumen a tus labios gracias a su brillo, y también mantendrá su humectación durante este periodo de tiempo.', 11702, '1739947447500.png'),
+(2, 'Base True Match Serum', 1, 1, 1, 1, 'Al instante ilumina, equilibra e hidrata, todo en uno para un radiante acabado en tono piel. 1% de ácido hialurónico, hidrata y suaviza al instante, pigmentos minerales luminosos para una cobertura ligera.', 25289, '1740026968570.png'),
+(4, 'Sombra en Barra Metallic Eyeshadow', 1, 3, 4, 6, 'Súper cremosa y de larga duración. Esta sombra en barra logra excelente cobertura en una sola pasada, mientras se siente liviana y confortable. Práctica a la hora de aplicar y trasladar, aporta un efecto metálico único. Probá las sombras con colores más intensos y brillantes.', 12490, '1739995569614.png'),
+(6, 'Labial CC Hidratante FPS 25 Una', 1, 1, 6, 7, 'El Labial CC Hidratante FPS 25 de Una posee colores llamativos, además de tratar tus labios con 10 beneficios increíbles. Contiene ingredientes de origen natural (sésamo, cacao y ceramidas de maracuyá), que combaten el envejecimiento y contribuyen para la firmeza, el volumen y la nutrición de los labios.', 21410, '1739996005044.png'),
+(7, 'Paleta de Sombras 12 Colores Starlight', 1, 3, 4, 6, 'Paleta de Sombras Extreme 12 Colores Starlight. Tener presente que algunos productos pueden venir sin termosellado.', 11693, '1739996167282.png'),
+(8, 'Superstay Vinyl Ink Unrivaled', 1, 1, 7, 1, 'El nuevo labial líquido SuperStay Vinyl Ink de Maybellline New York leda a tus labios un color vinilo de larga duración. Ahora disponible ennuevos tonos nude. Consigue un brillo instantáneo que dura hasta 16horas.', 12803, '1739996231618.png'),
+(15, 'Polvo Natural Bronzer', 1, 2, 3, 5, 'Este bronceador Rimmel London Natural Bronzer está diseñado para proporcionar un brillo cálido en todo el cuerpo y un aspecto natural y bronceado. Aplica el bronceador en las mejillas y las sienes para crear un brillo cálido y natural.', 1234, '1739994232058.png'),
+(18, 'Essencial Exclusivo Floral', 2, 4, 5, 7, 'Creación moderna, fuerte y al mismo tiempo misteriosa. Combina las especias frescas con el cuerpo de la madera y los ingredientes naturales inéditos brasileños. Un perfume exclusivo, que hace cada momento único. Camino olfativo: Intenso, jazmin, osmanthus', 11111, '1739995650597.png'),
+(51, 'Labial', 1, 1, 10, 9, 'lalalala', 11702, '1749599636067.png');
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,7 @@ INSERT INTO `articulos` (`ID_Articulo`, `Nombre`, `FK_Categoria`, `FK_Subcategor
 --
 
 CREATE TABLE `categorias` (
-  `Id_Categoria` int(10) NOT NULL,
+  `ID_Categoria` int(10) NOT NULL,
   `Nombre` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,9 +69,10 @@ CREATE TABLE `categorias` (
 -- Volcado de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`Id_Categoria`, `Nombre`) VALUES
+INSERT INTO `categorias` (`ID_Categoria`, `Nombre`) VALUES
 (1, 'Maquillaje'),
-(2, 'Fragancia');
+(2, 'Fragancia'),
+(5, 'Electrodomésticos');
 
 -- --------------------------------------------------------
 
@@ -95,19 +97,26 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`ID_Login`, `Nombre`, `Apellido`, `FK_Provincia`, `FK_Pronombre`, `Celular`, `Mail`, `Password`, `Imagen`) VALUES
-(1, 'Carla', '', 1, 3, 0, 'carla@mail.com', '123456', NULL),
-(2, 'Carlitos', '', 1, 1, 0, 'carlitos@mail.com', '654321', NULL),
-(3, 'Pepito', '', 1, 3, 0, 'pepito@mail.com', '$2a$08$ixOHS4eiYqqTf', NULL),
-(5, 'Laura', '', 1, 2, 0, 'laura@mail.com', '$2a$08$f0B.lyQOvBCyP', NULL),
-(6, 'Juan', '', 1, 1, 0, 'juan@mail.com', '$2a$08$X/F.Sicw.pB8V', NULL),
-(7, 'Martita', '', 1, 2, 0, 'martita@mail.com', '$2a$08$EAHhP5ciub6mA', NULL),
-(9, 'Armando', 'Cruz', 1, 1, 1122334455, 'armando@mail.com', '$2a$08$xTyzN69IVY5tV', ''),
+(1, 'carla', 'Rossi', 1, 1, 44771177, 'carla@mail.com', '$2a$08$oUC5V6pYldZGsQPYjvxUrOuMPeXoeFBR3Meo1LvgeUiVPW1LTwxl6', '1740448815432.jpg'),
+(2, 'Carlitos', 'aaa', 1, 2, 0, 'carlitos@mail.com', '$2a$08$T4uv2yT2Rm7Ui5PuaQ6XfuoHHLtVc1X6ppIs3VR45KalnpGHiubkW', '1739930265412.jpg'),
+(3, 'Pepito', 'c', 1, 1, 0, 'pepito@mail.com', '$2a$08$N98mmxGIicKBFDUUwB9tZOCswIlXQbUpn85/3GRRbPmOpPlvv9MDu', '1739930326959.png'),
 (11, 'Patricio', 'Rey', 1, 1, 11111111, '1@mail.com', '$2a$08$5jLRaVHciL1tH', '1730776662303.jpg'),
 (12, 'Luciana', 'Campos', 1, 3, 1544779452, 'luciana2@mail.com', '$2a$08$xvEqKHfN7a31G', '1730842947875.jpg'),
 (13, 'Mia', 'Wallace', 1, 2, 1544779966, 'miaw@mail.com', '$2a$08$27PhHUdUDBMpRnLQuLo0b.mpNgUsS2/4FUxd130Xdj.5BYwnQMcvC', '1730857405444.jpg'),
 (14, 'Marco', 'Polo', 1, 1, 1144337766, 'marco@mail.com', '$2a$08$z3zUPDx7ODD0fDM/BHjJ8..jl9n/GKVQJBy16QKwm6R5nVWqRVrTy', '1730857611114.jpg'),
-(15, 'Maria', 'Lopez', 1, 3, 1144663344, 'maria@mail.com', '$2a$08$vA/1ERNtOYuZxQ7bVmYDQuMyYzSVX7Aci2bxgwxNavp3ok1M1XwM.', '1730857776890.jpg'),
-(17, 'Marta', 'Lopez', 1, 2, 1166443377, 'marta@mail.com', '$2a$08$zXLatikb00aBxMDmh/rE8e1D57qLZuVKt0ztNXhXAit9OU7YRM3nK', '1730858157080.jpg');
+(17, 'Marta', 'Lopez', 1, 2, 1166443377, 'marta@mail.com', '$2a$08$zXLatikb00aBxMDmh/rE8e1D57qLZuVKt0ztNXhXAit9OU7YRM3nK', '1730858157080.jpg'),
+(18, 'Luna', 'Sanchez', 1, 2, 1122334477, 'luna@mail.com', '$2a$08$76U.gOlysYspC3ebFX7yae9BTFHlBe6EMGqPeMw7cQXl3wrPOTLhW', '1731178135436.JPG'),
+(19, 'Anita', 'Sanchez', 1, 2, 2147483647, 'anita@mail.com', '$2a$08$KUY250c6zFUvLs1M.jTjROnm9KCzMqkMF9nn9fhguGqa3296L4P7u', '1731178447018.jpg'),
+(21, 'Carla', 'Rossi', 1, 1, 111, 'carla3@mail.com', '$2a$08$hgkN6nVF3doIokmV/F512.7Dw1HbUqj44RzQK5y9KfK/ksKA9VSuW', '1739856914794.jpg'),
+(22, 'A', 'Rossi', 1, 1, 111111111, 'carla4@mail.com', '$2a$08$bpokeAm6QdzyPPhf6dpYAOcVlJzPPXePO/5wmF/gynccBfPr92QWq', '1739857028967.jpg'),
+(23, 'sas', 'a', 1, 2, 1, 'carla5@mail.com', '$2a$08$jjK2VpwxEkugKl7b.l1sZOpV9geNiXWrDHKyDHPYX1N3fi8K3U6p.', '1739857268786.jpg'),
+(24, 'aa', 'aaa', 2, 3, 2147483647, 'blabla@mail.com', '$2a$08$ypaMsPGuNH6wKgvuK/3jPO8J3afI0yPuyWWw.7PbhlIz36HBCrLmO', '1739857344044.jpg'),
+(27, 'sas', 'Rossi', 4, 1, 11, 'carla10@mail.com', '$2a$08$sVKabbKvm6r7JvReckBTWehFK59mr7A5Dx6byQ9K.2b7JmHQ/9vyK', '1739857708502.png'),
+(36, 'Carla', 'blabla', 1, 1, 1122334455, 'carla21212321@mail.com', '$2a$08$BfmJ0GWOZbK3Axc3XqkGmuJ8kUA.9BtTCrx1qfUIsxiZcjiCykPeu', '1740088530321.jpg'),
+(38, 'Carla', 'aaaa', 1, 1, 1111111, 'carla777777@mail.com', '$2a$08$YnuPoAh7rPLllil2CEfaieyMccq6aBGBLqMmW/0GYZsC3FdBpOVye', '1740713741776.jpg'),
+(40, 'dsadsads', 'dsadsadsa', 1, 1, 2147483647, 'anita33@mail.com', '$2a$08$LTR2MKS5nqFsoPkJYc5wmuPxLftnpGzd8cQ0STszd9vgNtj.ptd0W', '1741033246346.jpg'),
+(42, 'Pedro', 'Lopez', 1, 1, 1122664488, 'pedrito@mail.com', '$2a$08$wWIbBLtBewctZlBQwwKEm.Kbtc0ilX2tIM9m.pIaOTl23Avrl7D82', '1752107549814.jpg'),
+(43, 'Pablo', 'Gomez', 1, 1, 1144778866, 'pablo@mail.com', '$2a$08$ngCCj5cTQo7fbjLInwv1D.9GjUefpNENUp45P2.7yFeQXvwUxDBbO', '1752193505979.jpg');
 
 -- --------------------------------------------------------
 
@@ -134,7 +143,7 @@ INSERT INTO `marcas` (`ID_Marca`, `Nombre`, `FK_Pais`) VALUES
 (8, 'MAC', 6),
 (9, 'Lancôme', 2),
 (10, 'marcafalsa', 1),
-(13, 'Marcafalsa321', 1);
+(18, 'Ruby Rosee', 4);
 
 -- --------------------------------------------------------
 
@@ -157,7 +166,13 @@ INSERT INTO `paises` (`ID_Pais`, `Nombre`) VALUES
 (3, 'Inglaterra'),
 (4, 'Brasíl'),
 (5, 'Argentina'),
-(6, 'Canadá');
+(6, 'Canadá'),
+(8, 'Méjico'),
+(10, 'Holanda'),
+(12, 'Guatemala'),
+(13, 'China'),
+(20, 'Mongolia'),
+(25, 'Jamaica');
 
 -- --------------------------------------------------------
 
@@ -195,7 +210,9 @@ CREATE TABLE `provincias` (
 --
 
 INSERT INTO `provincias` (`ID_Provincia`, `Nombre`) VALUES
-(1, 'Buenos Aires');
+(1, 'Buenos Aires'),
+(2, 'Salta'),
+(4, 'Córdoba');
 
 -- --------------------------------------------------------
 
@@ -217,7 +234,8 @@ INSERT INTO `subcategorias` (`ID_Sub`, `Nombre`, `FK_Categoria`) VALUES
 (1, 'Labios', 1),
 (2, 'Piel', 1),
 (3, 'Ojos', 1),
-(4, 'Eau de Parfum', 2);
+(4, 'Eau de Parfum', 2),
+(11, 'Cejas', 1);
 
 -- --------------------------------------------------------
 
@@ -242,7 +260,9 @@ INSERT INTO `tipos` (`ID_Tipo`, `Nombre`, `FK_Subcategoria`) VALUES
 (4, 'Sombra', 3),
 (5, 'Floral', 4),
 (6, 'Hidratante', 1),
-(7, 'Liquido', 1);
+(7, 'Liquido', 1),
+(10, 'Matte', 1),
+(15, 'prueba', 1);
 
 --
 -- Índices para tablas volcadas
@@ -263,7 +283,7 @@ ALTER TABLE `articulos`
 -- Indices de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`Id_Categoria`);
+  ADD PRIMARY KEY (`ID_Categoria`);
 
 --
 -- Indices de la tabla `login`
@@ -322,55 +342,55 @@ ALTER TABLE `tipos`
 -- AUTO_INCREMENT de la tabla `articulos`
 --
 ALTER TABLE `articulos`
-  MODIFY `ID_Articulo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID_Articulo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `Id_Categoria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_Categoria` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `login`
 --
 ALTER TABLE `login`
-  MODIFY `ID_Login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID_Login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `ID_Marca` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID_Marca` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de la tabla `paises`
 --
 ALTER TABLE `paises`
-  MODIFY `ID_Pais` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_Pais` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `pronombres`
 --
 ALTER TABLE `pronombres`
-  MODIFY `ID_Pronombre` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_Pronombre` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `provincias`
 --
 ALTER TABLE `provincias`
-  MODIFY `ID_Provincia` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_Provincia` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `subcategorias`
 --
 ALTER TABLE `subcategorias`
-  MODIFY `ID_Sub` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID_Sub` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `tipos`
 --
 ALTER TABLE `tipos`
-  MODIFY `ID_Tipo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID_Tipo` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
